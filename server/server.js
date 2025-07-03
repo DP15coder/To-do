8d3e4d97f304f664b0fd49623af11183520712bf
Draksha Patle <drakshapatle15@gmail.com>
2025-07-03 17:05:04 +0530

    candidate fix for cors error

--- server/server.js
+++ server/server.js
@@ -7,17 +7,29 @@ const todoRoutes = require('./routes/todoRoutes');
 const app = express();
 app.use(express.json());
 const PORT = process.env.PORT;
+
+const allowedOrigins = [
+  'http://localhost:5173',
+  process.env.FRONTEND_HOST
+];
+
 app.use(cors({
-  origin: 'http://localhost:5173',
-  credentials: true, 
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
 }));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/todos', todoRoutes);
 // app.use(errorHandler);
 
-app.listen(PORT, () =>{
-    connectDB();
-  console.log(`Server is listeninmg on port ${PORT}`)
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server is listening on port ${PORT}`)
 }
 );
